f5dc5c3290f38e5420f7189e152dcda08a2a5231
Piero Juarez <pierojuarez10@gmail.com>
2026-06-30 04:01:28 -0500

    nuevo pull

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print("Bienvenido al sistema de la tienda")
 print("Sistema de ventas iniciado")
 print("Usuario autenticado")
 print("Bienvenido, ¿en qué puedo ayudarte hoy?")
-Nombre = "Piero"
+Nombre = "Juan"
 print(Nombre)
