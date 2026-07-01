1b073c66eabc2e84b7ed9fe00930f3afb1573574
Piero Juarez <pierojuarez10@gmail.com>
2026-06-30 19:34:23 -0500

    Cambio para main

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ print("Bienvenido al sistema de la tienda")
 print("Sistema de ventas iniciado")
 print("Usuario autenticado")
 "Esto es un cambio para main"
-"Esto es otro cambio para main"
+"Esto es otro cambio para main y acabo de crear un conflicto"
+"Este es el tercer cambio para unir con main"
